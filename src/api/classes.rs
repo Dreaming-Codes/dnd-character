636a9f38a52705d453a455cddd0c6f96b7d67451
DreamingCodes <me@dreaming.codes>
2023-12-15 22:00:30 +0100

    Added ability slots query

--- src/api/classes.rs
+++ src/api/classes.rs
@@ -34,6 +34,49 @@ struct AbilityScore {
     pub index: String,
 }
 
+#[derive(cynic::QueryVariables, Debug)]
+pub struct SpellcastingQueryVariables {
+    pub index: Option<String>,
+}
+
+#[derive(cynic::QueryFragment, Debug)]
+#[cynic(graphql_type = "Query", variables = "SpellcastingQueryVariables")]
+pub struct SpellcastingQuery {
+    #[arguments(index: $index)]
+    pub level: Option<Level>,
+}
+
+#[derive(cynic::QueryFragment, Debug)]
+pub struct Level {
+    pub spellcasting: Option<LevelSpellcasting>,
+}
+
+#[derive(cynic::QueryFragment, Debug)]
+pub struct LevelSpellcasting {
+    #[cynic(rename = "cantrips_known")]
+    pub cantrips_known: Option<i32>,
+    #[cynic(rename = "spell_slots_level_1")]
+    pub spell_slots_level_1: Option<i32>,
+    #[cynic(rename = "spell_slots_level_2")]
+    pub spell_slots_level_2: Option<i32>,
+    #[cynic(rename = "spell_slots_level_3")]
+    pub spell_slots_level_3: Option<i32>,
+    #[cynic(rename = "spell_slots_level_4")]
+    pub spell_slots_level_4: Option<i32>,
+    #[cynic(rename = "spell_slots_level_5")]
+    pub spell_slots_level_5: Option<i32>,
+    #[cynic(rename = "spell_slots_level_6")]
+    pub spell_slots_level_6: Option<i32>,
+    #[cynic(rename = "spell_slots_level_7")]
+    pub spell_slots_level_7: Option<i32>,
+    #[cynic(rename = "spell_slots_level_8")]
+    pub spell_slots_level_8: Option<i32>,
+    #[cynic(rename = "spell_slots_level_9")]
+    pub spell_slots_level_9: Option<i32>,
+}
+
+
+
 impl Class {
     pub async fn get_spellcasting_ability_index(&self) -> Result<String, ApiError> {
         let op = SpellcastingAbilityQuery::build(SpellcastingAbilityQueryVariables {
@@ -49,5 +92,30 @@ impl Class {
             .spellcasting_ability.index;
 
         Ok(ability_index)
+    }
+
+    pub async fn get_spellcasting_slots(&self) -> Result<LevelSpellcasting, ApiError> {
+        let op = SpellcastingQuery::build(SpellcastingQueryVariables {
+            index: Some(format!("{}-{}", self.0, self.1.level))
+        });
+
+        let spellcasting_slots = Client::new()
+            .post("https://www.dnd5eapi.co/graphql")
+            .run_graphql(op).await?
+            .data.ok_or(ApiError::Schema)?
+            .level.ok_or(ApiError::Schema)?
+            .spellcasting.ok_or(ApiError::Schema)?;
+
+        Ok(spellcasting_slots)
+    }
+
+    pub async fn apply_level(&mut self, level: u8) -> Result<(), ApiError> {
+
+        // If feature index contain ability-sucore-improvement, then trigger the ability score improvement
+        // If the feature is spell-mastery, then add the description to the other field of the class and in the very next element of the vector add the spell to use with spell mastery (instruct the ai)
+        // When the feature is signature-spell then add the description to the other field of the class and in the very next element of the vector add the spell to use with signature spell (instruct the ai)
+
+        self.1.level = level;
+        Ok(())
     }
 }
