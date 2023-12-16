9386205ef6202e0bb95e0148d4561f7964b2fcb2
DreamingCodes <me@dreaming.codes>
2023-12-16 17:46:58 +0100

    Restructured class logic to hashmap instead of hashset to permit mutability

--- src/classes.rs
+++ src/classes.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::hash::Hash;
 use serde::{Deserialize, Serialize};
 
@@ -29,7 +29,7 @@ impl Default for ClassProperties {
 /// The key is the index of the class from https://www.dnd5eapi.co/api/classes
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[cfg_attr(feature = "serde", serde(rename_all = "camelCase"))]
-pub struct Class(pub String, pub ClassProperties);
+pub struct Class(String, pub ClassProperties);
 
 impl Hash for Class {
     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
@@ -45,20 +45,22 @@ impl PartialEq for Class {
 
 impl Eq for Class {}
 
+impl Class {
+    pub fn index(&self) -> &str {
+        &self.0
+    }
+}
+
 #[derive(Default)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[cfg_attr(feature = "serde", serde(rename_all = "camelCase"))]
-pub struct Classes(pub HashSet<Class>);
+pub struct Classes(pub HashMap<String, Class>);
 
 
 impl Classes {
-    pub fn get(&self, class: &str) -> Option<&Class> {
-        self.0.get(&Class(class.to_string(), ClassProperties::default()))
-    }
-
     pub fn new(class_index: String) -> Self {
         let mut classes = Self::default();
-        classes.0.insert(Class(class_index, ClassProperties::default()));
+        classes.0.insert(class_index.clone(), Class(class_index, ClassProperties::default()));
         classes
     }
 }
