39047db8a30a7a32c198ea5e4d8bc832ce3d50ad
DreamingCodes <me@dreaming.codes>
2024-02-27 20:18:17 +0100

    Fixed tests

--- tests/api_test.rs
+++ tests/api_test.rs
@@ -4,7 +4,7 @@ use dnd_character::Character;
 
 #[tokio::test]
 async fn get_level_features(){
-    let mut dnd_character = Character::new("cleric".to_string(), "a".to_string(), 16,"human".to_string(), "human".to_string(), "chaotic-neutral".to_string(), "bard".to_string(), "".to_string(), "".to_string()).await.expect("Error in API Request");
+    let mut dnd_character = Character::new("cleric".to_string(), "a".to_string(), 16,"human".to_string(), "human".to_string(), "chaotic-neutral".to_string(), "bard".to_string(), "".to_string(), "".to_string());
 
     dnd_character.add_experience(90000);
 
@@ -12,7 +12,7 @@ async fn get_level_features(){
 
     assert_eq!(features.iter().filter(|feature| feature.starts_with("destroy-undead-cr-")).count(), 1);
 
-    let mut dnd_character = Character::new("bard".to_string(), "a".to_string(), 16,"human".to_string(), "human".to_string(), "chaotic-neutral".to_string(), "bard".to_string(), "".to_string(), "".to_string()).await.expect("Error in API Request");
+    let mut dnd_character = Character::new("bard".to_string(), "a".to_string(), 16,"human".to_string(), "human".to_string(), "chaotic-neutral".to_string(), "bard".to_string(), "".to_string(), "".to_string());
 
     dnd_character.add_experience(90000);
 
